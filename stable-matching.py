59871608c2f7f47a3f2df3027a336e74ef65da2b
Breno Calazans <brenocalazans@gmail.com>
2015-03-17 23:09:46 -0300

    Add stable matching algorithm

---
+++ stable-matching.py
@@ -0,0 +1,114 @@
+# Stable Matching - Gale-Shapley Algorithm
+
+# Let the input be:
+
+# Men
+# Tom	Jen Nic Lis
+# Mat	Nic Lis Jen
+# Ken	Jen	Lis	Nic
+men = {
+	'Tom': ['Jen', 'Nic', 'Lis'],
+	'Mat': ['Nic', 'Lis', 'Jen'],
+	'Ken': ['Jen', 'Lis', 'Nic']
+}
+
+# Women
+# Jen	Tom	Mat	Ken
+# Lis	Ken Mat Tom
+# Nic	Mat Ken Tom
+women = {
+	'Jen': ['Tom', 'Mat', 'Ken'],
+	'Lis': ['Ken', 'Mat', 'Tom'],
+	'Nic': ['Mat', 'Ken', 'Tom']
+}
+
+class Person:
+	def __init__(self, name, preferences):
+		self.name = name
+		self.preferences = preferences
+
+		self.engaged_to = None
+		self.proposed_to = []
+
+	def __str__(self):
+		if self.engaged_to:
+			return self.name + ' is engaged to ' + self.engaged_to
+
+	def find_match_not_proposed(self):
+		for woman in self.preferences:
+			if not (woman in self.proposed_to):
+				return woman
+		return None
+
+	def would_rather(self, a, b):
+		if self.preferences.index(a) < self.preferences.index(b):
+			return True
+		else:
+			return False
+
+def stable_matching (men, women):
+	# Initialize all men and women to free
+	free_men_names = []
+
+	men_dict = {}
+	for name in men:
+		free_men_names.append(name)
+		man = Person(name, men[name])
+		men_dict[name] = man
+
+	women_dict = {}
+	for name in women:
+		woman = Person(name, women[name])
+		women_dict[name] = woman
+
+	free_man_name = free_men_names.pop()
+
+	# While exists a free man m who still has a woman w to propose to
+	while free_man_name:
+
+		# Get highest ranked woman to whom m has not yet proposed
+		man = men_dict[free_man_name]
+		woman_name = man.find_match_not_proposed()		
+		woman = women_dict[woman_name]
+		
+		# If w is free
+		if woman.engaged_to is None:
+
+			# (m, w) become engaged
+			woman.engaged_to = free_man_name
+			man.engaged_to = woman_name
+
+		else: # some pair (m', w) already exists
+
+			# If w prefers m to m'
+			if woman.would_rather(free_man_name, woman.engaged_to):
+				# (m, w) become engaged
+				ex_man_name = woman.engaged_to
+				woman.engaged_to = free_man_name
+				man.engaged_to = woman_name
+
+				# m' becomes free
+				ex_man = men_dict[ex_man_name]
+				ex_man.engaged_to = None
+				free_men_names.append(ex_man_name)
+			else:
+				# m will propose to the next woman
+				free_men_names.append(free_man_name)
+
+		man.proposed_to.append(woman_name)
+
+		if free_men_names:
+			free_man_name = free_men_names.pop()
+		else:
+			free_man_name = None
+
+	for name in men_dict:
+		print men_dict[name]
+
+if __name__ == '__main__':
+	stable_matching(men, women)
+
+
+
+
+
