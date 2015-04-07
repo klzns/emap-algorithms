40a93cfd891f194fc0b7ad678e3a4bcb678407b8
Breno Calazans <breno@vtex.com.br>
2015-04-07 12:10:42 -0300

    Add comments to linked list implementation

--- list-4/linked_list.py
+++ list-4/linked_list.py
@@ -1,35 +1,43 @@
 class Node:
     def __init__(self, cargo=None, next=None):
+        # Value of the node
         self.car = cargo
+        # Link to the next element in the list
         self.cdr = next
 
-    def distance(self, end_value):
+    # Return the distance of the node until the other
+    # This method assumes that the list has unique elements
+    # E.g. if used on the first element of the list with the second parameter
+    # as None it will return the length of the list
+    def distance(self, end_node):
         node = self
         has_next = None
         length = 0
 
         length = length + 1
-        while node.cdr and node.car is not end_value:
+        while node.cdr and node.car is not end_node:
             node = node.cdr
             length = length + 1
 
         return length
 
 
+# Transform an array to a linked list
 class List:
     def __init__(self, values):
         first = None
-        oldNode = None
+        previous_node = None
 
         for value in values:
-            if oldNode:
-                oldNode.cdr = node = Node(value)
+            # Link the previous node with the current node
+            if previous_node:
+                previous_node.cdr = node = Node(value)
+            # In case is the first node of the list
             else:
                 node = Node(value)
-
-            if first is None:
                 first = node
 
-            oldNode = node
+            previous_node = node
 
+        # The only attribute this object has is a link to the first element
         self.first = first
