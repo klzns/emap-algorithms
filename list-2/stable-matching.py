fb61e10a103bec10158edd9216bef1d51521e400
Breno Calazans <breno@vtex.com.br>
2015-04-17 13:51:10 -0300

    Lint

--- list-2/stable-matching.py
+++ list-2/stable-matching.py
@@ -3,112 +3,116 @@
 # Let the input be:
 
 # Men
-# Tom	Jen Nic Lis
-# Mat	Nic Lis Jen
-# Ken	Jen	Lis	Nic
+# Tom   Jen Nic Lis
+# Mat   Nic Lis Jen
+# Ken   Jen Lis Nic
 men = {
-	'Tom': ['Jen', 'Nic', 'Lis'],
-	'Mat': ['Nic', 'Lis', 'Jen'],
-	'Ken': ['Jen', 'Lis', 'Nic']
+    'Tom': ['Jen', 'Nic', 'Lis'],
+    'Mat': ['Nic', 'Lis', 'Jen'],
+    'Ken': ['Jen', 'Lis', 'Nic']
 }
 
 # Women
-# Jen	Tom	Mat	Ken
-# Lis	Ken Mat Tom
-# Nic	Mat Ken Tom
+# Jen   Tom Mat Ken
+# Lis   Ken Mat Tom
+# Nic   Mat Ken Tom
 women = {
-	'Jen': ['Tom', 'Mat', 'Ken'],
-	'Lis': ['Ken', 'Mat', 'Tom'],
-	'Nic': ['Mat', 'Ken', 'Tom']
+    'Jen': ['Tom', 'Mat', 'Ken'],
+    'Lis': ['Ken', 'Mat', 'Tom'],
+    'Nic': ['Mat', 'Ken', 'Tom']
 }
 
+
 class Person:
-	def __init__(self, name, preferences):
-		self.name = name
-		self.preferences = preferences
+    def __init__(self, name, preferences):
+        self.name = name
+        self.preferences = preferences
 
-		self.engaged_to = None
-		self.proposed_to = []
+        self.engaged_to = None
+        self.proposed_to = []
 
-	def find_match_not_proposed(self):
-		for woman in self.preferences:
-			if not (woman in self.proposed_to):
-				return woman
-		return None
+    def find_match_not_proposed(self):
+        for woman in self.preferences:
+            if not (woman in self.proposed_to):
+                return woman
+        return None
 
-	def would_rather(self, a, b):
-		if self.preferences.index(a) < self.preferences.index(b):
-			return True
-		else:
-			return False
+    def would_rather(self, a, b):
+        if self.preferences.index(a) < self.preferences.index(b):
+            return True
+        else:
+            return False
 
-def stable_matching (men, women):
-	# Initialize all men and women to free
-	free_men_names = []
 
-	men_dict = {}
-	for name in men:
-		free_men_names.append(name)
-		man = Person(name, men[name])
-		men_dict[name] = man
+def stable_matching(men, women):
+    # Initialize all men and women to free
+    free_men_names = []
 
-	women_dict = {}
-	for name in women:
-		woman = Person(name, women[name])
-		women_dict[name] = woman
+    men_dict = {}
+    for name in men:
+        free_men_names.append(name)
+        man = Person(name, men[name])
+        men_dict[name] = man
 
-	free_man_name = free_men_names.pop()
+    women_dict = {}
+    for name in women:
+        woman = Person(name, women[name])
+        women_dict[name] = woman
 
-	# While exists a free man m who still has a woman w to propose to
-	while free_man_name:
+    free_man_name = free_men_names.pop()
 
-		# Get highest ranked woman to whom m has not yet proposed
-		man = men_dict[free_man_name]
-		woman_name = man.find_match_not_proposed()		
-		woman = women_dict[woman_name]
-		
-		# If w is free
-		if woman.engaged_to is None:
+    # While exists a free man m who still has a woman w to propose to
+    while free_man_name:
 
-			# (m, w) become engaged
-			woman.engaged_to = free_man_name
-			man.engaged_to = woman_name
+        # Get highest ranked woman to whom m has not yet proposed
+        man = men_dict[free_man_name]
+        woman_name = man.find_match_not_proposed()
+        woman = women_dict[woman_name]
 
-		else: # some pair (m', w) already exists
+        # If w is free
+        if woman.engaged_to is None:
 
-			# If w prefers m to m'
-			if woman.would_rather(free_man_name, woman.engaged_to):
-				# (m, w) become engaged
-				ex_man_name = woman.engaged_to
-				woman.engaged_to = free_man_name
-				man.engaged_to = woman_name
+            # (m, w) become engaged
+            woman.engaged_to = free_man_name
+            man.engaged_to = woman_name
 
-				# m' becomes free
-				ex_man = men_dict[ex_man_name]
-				ex_man.engaged_to = None
-				free_men_names.append(ex_man_name)
-			else:
-				# m will propose to the next woman
-				free_men_names.append(free_man_name)
+        else:  # some pair (m', w) already exists
 
-		man.proposed_to.append(woman_name)
+            # If w prefers m to m'
+            if woman.would_rather(free_man_name, woman.engaged_to):
+                # (m, w) become engaged
+                ex_man_name = woman.engaged_to
+                woman.engaged_to = free_man_name
+                man.engaged_to = woman_name
 
-		if free_men_names:
-			free_man_name = free_men_names.pop()
-		else:
-			free_man_name = None
+                # m' becomes free
+                ex_man = men_dict[ex_man_name]
+                ex_man.engaged_to = None
+                free_men_names.append(ex_man_name)
+            else:
+                # m will propose to the next woman
+                free_men_names.append(free_man_name)
 
-	pairs = []
-	for man in men_dict:
-		pairs.append([man, men_dict[man].engaged_to])
+        man.proposed_to.append(woman_name)
 
-	return pairs
+        if free_men_names:
+            free_man_name = free_men_names.pop()
+        else:
+            free_man_name = None
+
+    pairs = []
+    for man in men_dict:
+        pairs.append([man, men_dict[man].engaged_to])
+
+    return pairs
+
 
 def print_pair(pair):
-	print pair[0] + ' is engaged to ' + pair[1]
+    print pair[0] + ' is engaged to ' + pair[1]
+
 
 if __name__ == '__main__':
-	pairs = stable_matching(men, women)
+    pairs = stable_matching(men, women)
 
-	for pair in pairs:
-		print_pair(pair)
+    for pair in pairs:
+        print_pair(pair)
--- list-3/merge-sort.py
+++ list-3/merge-sort.py
@@ -4,28 +4,27 @@ list = [5, 2, 69, 21, 123, 1, 4]
 
 
 def merge(list_a, list_b):
-	if len(list_a) is 0:
-		return list_b	
-	if len(list_b) is 0:
-		return list_a
+    if len(list_a) is 0:
+        return list_b
+    if len(list_b) is 0:
+        return list_a
 
-	if list_a[0] < list_b[0]:
-		return [list_a[0]] + list_a[1:] + list_b[:]
-	else:
-		return [list_b[0]] + list_a[:] + list_b[1:]
+    if list_a[0] < list_b[0]:
+        return [list_a[0]] + list_a[1:] + list_b[:]
+    else:
+        return [list_b[0]] + list_a[:] + list_b[1:]
 
 
 def merge_sort(list):
-	queue = deque([])
+    queue = deque([])
 
-	for i in range(0, len(list)):
-		queue.append([list[i]])
+    for i in range(0, len(list)):
+        queue.append([list[i]])
 
-	while len(queue) > 1:
-		queue.append(merge(queue.popleft(), queue.popleft()))
+    while len(queue) > 1:
+        queue.append(merge(queue.popleft(), queue.popleft()))
 
-	return queue.popleft()
+    return queue.popleft()
 
 if __name__ == '__main__':
-	print merge_sort(list)
-
+    print merge_sort(list)
